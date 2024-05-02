848fcd090cad03e69314c08fc3e0cedf41c43fa6
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-05-02 16:42:23 -0300

    first commit

---
+++ src/main/kotlin/ingsis/tricolor/Permission/PermissionApplication.kt
@@ -0,0 +1,11 @@
+package ingsis.tricolor.Permission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PermissionApplication
+
+fun main(args: Array<String>) {
+	runApplication<PermissionApplication>(*args)
+}
---
+++ src/test/kotlin/ingsis/tricolor/Permission/PermissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsis.tricolor.Permission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PermissionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
