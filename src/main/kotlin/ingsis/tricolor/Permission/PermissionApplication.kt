167eda7ffe7e244a6e07041b3a2ad8049066cc50
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-06-03 15:01:53 -0300

    Working authentication

--- src/main/kotlin/ingsis/tricolor/Permission/PermissionApplication.kt
+++ src/main/kotlin/ingsis/tricolor/Permission/PermissionApplication.kt
@@ -2,10 +2,32 @@ package ingsis.tricolor.Permission
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @SpringBootApplication
-class PermissionApplication
+class PermissionApplication {
+    @GetMapping("/")
+    fun noAuth(): String {
+        return "No need for auth"
+    }
+
+    @GetMapping("/jwt")
+    fun getAuth(
+        @AuthenticationPrincipal jwt: Jwt,
+    ): String {
+        return jwt.tokenValue
+    }
+
+    @GetMapping("/needs-auth")
+    fun needsAuth(): String {
+        return "Great! you are authenticated"
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<PermissionApplication>(*args)
+    runApplication<PermissionApplication>(*args)
 }
