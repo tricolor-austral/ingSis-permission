25a2d47d8cb26288235cfcb37a58f2ed0b3f79a2
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-06-06 11:01:53 -0300

    fix: .env file doesn't work harcoded user password for DB feat: made simple conection with sinppet-holder api

--- src/main/kotlin/ingsis/tricolor/Permission/PermissionApplication.kt
+++ src/main/kotlin/ingsis/tricolor/Permission/PermissionApplication.kt
@@ -1,18 +1,48 @@
 package ingsis.tricolor.Permission
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @RestController
 @SpringBootApplication
-class PermissionApplication {
+class PermissionApplication(
+    @Autowired val template: RestTemplate,
+) {
     @GetMapping("/")
-    fun noAuth(): String {
-        return "No need for auth"
+    fun noAuth(
+        @RequestHeader("Authorization") authorizationHeader: String,
+    ): String {
+        val authToken = authorizationHeader
+        println(authToken)
+
+        // Create headers
+        val headers = HttpHeaders()
+        headers.set("Authorization", authToken)
+
+        // Create the HttpEntity with headers
+        val entity = HttpEntity<String>(headers)
+
+        // Make the HTTP GET request with headers
+        val response: ResponseEntity<String> =
+            template.exchange(
+                "http://localhost:8081/",
+                HttpMethod.GET,
+                entity,
+                String::class.java,
+            )
+
+        return response.body ?: "No response"
     }
 
     @GetMapping("/jwt")
