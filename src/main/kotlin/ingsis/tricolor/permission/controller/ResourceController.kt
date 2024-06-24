ab3899581948c185ede8b69c131396f68e56b7c1
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-06-24 19:08:00 -0300

    linter added

--- src/main/kotlin/ingsis/tricolor/permission/controller/ResourceController.kt
+++ src/main/kotlin/ingsis/tricolor/permission/controller/ResourceController.kt
@@ -9,7 +9,16 @@ import ingsis.tricolor.permission.service.interfaces.ResourceService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CookieValue
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.context.request.WebRequest
 
 @RestController()
